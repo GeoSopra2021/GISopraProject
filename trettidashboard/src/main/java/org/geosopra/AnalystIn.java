6c1f9fd30303b772e80257c384a20f38fcbfcae3
Maike Schröder <maikeschroder@wl-pool2-pnt-01318.uni-muenster.de>
2021-09-07 14:34:04 +0200

    Durchschnitt und Analystin static fix

--- trettidashboard/src/main/java/org/geosopra/AnalystIn.java
+++ trettidashboard/src/main/java/org/geosopra/AnalystIn.java
@@ -1,9 +1,13 @@
 package org.geosopra;
 
 public interface AnalystIn {
+<<<<<<< Updated upstream
 	
 	public double analyse(Datapoint[] dp);
 	
 	public abstract double getValue(Datapoint dp);
 
+=======
+    public double analyse(Datapoint[] dp);
+>>>>>>> Stashed changes
 }
