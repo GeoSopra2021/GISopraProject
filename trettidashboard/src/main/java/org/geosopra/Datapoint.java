c84f6fd553c9f56e85fb720d87cc669d3d0c7531
Lorano98 <evalangstein@freenet.de>
2021-09-07 12:29:36 +0200

    Create Datapoint.java

---
+++ trettidashboard/src/main/java/org/geosopra/Datapoint.java
@@ -0,0 +1,110 @@
+package org.geosopra;
+
+public class Datapoint {
+	
+	private long startTime;
+	private long endTime;
+	private long startX;
+	private long startY;
+	private long endX;
+	private long endY;
+	private double distance;
+	private double time;
+	/**
+	 * @return the distance
+	 */
+	public double getDistance() {
+		return distance;
+	}
+	/**
+	 * @param distance the distance to set
+	 */
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	/**
+	 * @return the startTime
+	 */
+	public long getStartTime() {
+		return startTime;
+	}
+	/**
+	 * @param startTime the startTime to set
+	 */
+	public void setStartTime(long startTime) {
+		this.startTime = startTime;
+	}
+	/**
+	 * @return the endTime
+	 */
+	public long getEndTime() {
+		return endTime;
+	}
+	/**
+	 * @param endTime the endTime to set
+	 */
+	public void setEndTime(long endTime) {
+		this.endTime = endTime;
+	}
+	/**
+	 * @return the startX
+	 */
+	public long getStartX() {
+		return startX;
+	}
+	/**
+	 * @param startX the startX to set
+	 */
+	public void setStartX(long startX) {
+		this.startX = startX;
+	}
+	/**
+	 * @return the startY
+	 */
+	public long getStartY() {
+		return startY;
+	}
+	/**
+	 * @param startY the startY to set
+	 */
+	public void setStartY(long startY) {
+		this.startY = startY;
+	}
+	/**
+	 * @return the endX
+	 */
+	public long getEndX() {
+		return endX;
+	}
+	/**
+	 * @param endX the endX to set
+	 */
+	public void setEndX(long endX) {
+		this.endX = endX;
+	}
+	/**
+	 * @return the endY
+	 */
+	public long getEndY() {
+		return endY;
+	}
+	/**
+	 * @param endY the endY to set
+	 */
+	public void setEndY(long endY) {
+		this.endY = endY;
+	}
+	/**
+	 * @return the time
+	 */
+	public double getTime() {
+		return time;
+	}
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(double time) {
+		this.time = time;
+	}
+
+}
