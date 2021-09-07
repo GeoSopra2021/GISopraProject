c3109a7ff201783d29730cce90c651b1d2324183
Maike Schröder <maikeschroder@wl-pool2-pnt-01318.uni-muenster.de>
2021-09-07 14:44:49 +0200

    Analystin fix

--- trettidashboard/src/main/java/org/geosopra/AnalystIn.java
+++ trettidashboard/src/main/java/org/geosopra/AnalystIn.java
@@ -1,13 +1,9 @@
 package org.geosopra;
 
 public interface AnalystIn {
-<<<<<<< Updated upstream
-	
+
 	public double analyse(Datapoint[] dp);
 	
 	public abstract double getValue(Datapoint dp);
 
-=======
-    public double analyse(Datapoint[] dp);
->>>>>>> Stashed changes
 }
