6fa05dbe241999efa14c5fa1cf310f1390a58dc9
Timo Lietmeyer <Timo123456789>
2021-09-07 14:50:02 +0200

    debugg format function

--- trettidashboard/src/main/java/org/geosopra/App.java
+++ trettidashboard/src/main/java/org/geosopra/App.java
@@ -15,35 +15,38 @@ public class App {
         // add personal path in String
         Scanner sc = new Scanner(new File(
                 "C:\\Users\\Timo\\OneDrive - Universität Münster (1)\\6. Fachsemester_SS_2021\\GISopraProject\\routes_bikes.csv"));
-        sc.useDelimiter(","); // sets the delimiter pattern
+        sc.useDelimiter("\n"); // sets the delimiter pattern
+        sc.next();
         while (sc.hasNext()) // returns a boolean value
         {
             format(sc.next());
             System.out.print(sc.next()); // find and returns the next complete token from this scanner
+            
         }
         sc.close(); // closes the scanner
     }
 
     public static void format(String date) {
         int Start_time_end_index = date.indexOf(';');
-        int End_time_right_index = date.indexOf(';', Start_time_end_index);
-        int Start_X_right_index = date.indexOf(';',End_time_right_index);
-        int Start_Y_right_index = date.indexOf(';', Start_X_right_index);
-        int End_X_right_index = date.indexOf(';', Start_Y_right_index);
-        int End_Y_right_index = date.indexOf(';', End_X_right_index);
-        int distance_right_index = date.indexOf(';', End_Y_right_index);
+        int End_time_right_index = date.indexOf(';', Start_time_end_index+1);
+        int Start_X_right_index = date.indexOf(';',End_time_right_index+1);
+        int Start_Y_right_index = date.indexOf(';', Start_X_right_index+1);
+        int End_X_right_index = date.indexOf(';', Start_Y_right_index+1);
+        int End_Y_right_index = date.indexOf(';', End_X_right_index+1);
+        int distance_right_index = date.indexOf(';', End_Y_right_index+1);
        
 
-
         String starttime = date.substring(0, Start_time_end_index);
-        String endtime = date.substring(Start_time_end_index, End_time_right_index);
-        String start_x = date.substring(End_time_right_index, Start_X_right_index);
-        String start_y = date.substring(Start_X_right_index, Start_Y_right_index);
-        String end_X = date.substring(Start_Y_right_index, End_X_right_index);
-        String end_Y = date.substring(End_X_right_index, End_Y_right_index);
-        String distance = date.substring(End_Y_right_index);
+        String endtime = date.substring(Start_time_end_index+1, End_time_right_index);
+        String start_x = date.substring(End_time_right_index+1, Start_X_right_index);
+        String start_y = date.substring(Start_X_right_index+1, Start_Y_right_index);
+        String end_X = date.substring(Start_Y_right_index+1, End_X_right_index);
+        String end_Y = date.substring(End_X_right_index+1, End_Y_right_index);
+        String distance = date.substring(End_Y_right_index+1,distance_right_index);
+        String Time_in_Hours = date.substring(distance_right_index+1);
 
         Datapoint row =new Datapoint();
+      
         row.setDistance(Integer.parseInt(distance));
         row.setEndTime(Integer.parseInt(endtime));
         row.setEndX(Integer.parseInt(end_X));
@@ -51,7 +54,9 @@ public class App {
         row.setStartTime(Integer.parseInt(starttime));
         row.setStartX(Integer.parseInt(start_x));
         row.setStartY(Integer.parseInt(start_y));
-        row.setTime(Integer.parseInt(endtime)-Integer.parseInt(starttime));
+        row.setTime(Integer.parseInt(Time_in_Hours));
+        
+        
 
         System.out.println(row);
 
