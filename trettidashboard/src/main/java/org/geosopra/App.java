d6203b162f57a8802161e915f5038c5968a33b00
Maike Schröder <maikeschroder@wl-pool2-pnt-01318.uni-muenster.de>
2021-09-07 14:22:07 +0200

    Main Methode für Distanz

--- trettidashboard/src/main/java/org/geosopra/App.java
+++ trettidashboard/src/main/java/org/geosopra/App.java
@@ -6,8 +6,23 @@ package org.geosopra;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+       Datapoint[] dp = new Datapoint[6];
+       dp[0] = new Datapoint();
+       dp[0].setDistance(3);
+       dp[1] = new Datapoint();
+       dp[1].setDistance(2);
+       dp[2] = new Datapoint();
+       dp[2].setDistance(2);
+       dp[3] = new Datapoint();
+       dp[3].setDistance(1);
+       dp[4] = new Datapoint();
+       dp[4].setDistance(4);
+       dp[5] = new Datapoint();
+       dp[5].setDistance(6);
+    AnalystIn entfernung = new Durchschnitt.DistanzDurchschnitt();
+    double ergebnis = entfernung.analyse(dp);
+    System.out.println("Die Durchschnittslänge ist " +ergebnis + " km");
+
     }
 }
