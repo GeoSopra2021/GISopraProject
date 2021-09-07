4f43bbce72e173ad81a9326e7585a6c77507cb3f
Lorano98 <evalangstein@freenet.de>
2021-09-07 13:47:07 +0200

    Durchschnitt.java created. Implements Datapoint.java

---
+++ trettidashboard/src/main/java/org/geosopra/Durchschnitt.java
@@ -0,0 +1,49 @@
+package org.geosopra;
+
+public abstract class Durchschnitt implements AnalystIn {
+
+	@Override
+	public double analyse(Datapoint[] dp) {
+		double ergebnis = 0;
+		for(int i = 0; i < dp.length; i++) {
+			ergebnis += getValue(dp[i]);
+		}
+		
+		ergebnis /= dp.length;
+		return ergebnis;
+	}
+	
+	protected abstract double getValue(Datapoint dp);
+	
+	public class DistanzDurchschnitt extends Durchschnitt {
+
+		@Override
+		protected double getValue(Datapoint dp) {
+			return dp.getDistance();
+		}
+
+	}
+	
+	public class ZeitDurchschnitt extends Durchschnitt {
+
+		@Override
+		protected double getValue(Datapoint dp) {
+			return dp.getTime();
+		}
+
+	}
+	
+	public class GeschwindigkeitDurchschnitt extends Durchschnitt {
+
+		@Override
+		/**
+		 * @return Durchschnittsgeschwindigkeit in km/h
+		 */
+		protected double getValue(Datapoint dp) {
+			return dp.getDistance() / dp.getTime();
+		}
+
+	}
+
+
+}
