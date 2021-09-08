f4f7c5e75fdde3fad2298a5c0aac7914e815ddad
Timo Lietmeyer <48100977+Timo123456789@users.noreply.github.com>
2021-09-08 13:01:35 +0200

    add_modell_attributes

--- trettidashboard/src/main/java/org/geosopra/Html_controller.java
+++ trettidashboard/src/main/java/org/geosopra/Html_controller.java
@@ -9,6 +9,26 @@ public class Html_controller {
 
     @RequestMapping("/")
     public String welcome(Model model) {
+        
+        model.addAttribute("Distanzdurchschnitt", 4.2);
+        model.addAttribute("Zeitdurchschnitt", 3);
+        model.addAttribute("Geschwindigkeitdurchschnitt", 3);
+        model.addAttribute("Kosten_Tretty", 3);
+        model.addAttribute("Kosten_Lime", 3);
+        model.addAttribute("Kosten_Tier", 3);
+        model.addAttribute("CO2_Car", 3);
+        model.addAttribute("CO2_Scooter", 3);
+        model.addAttribute("CO2_Bicycle", 3);
+        model.addAttribute("Ersparnis_Car", 3);
+        model.addAttribute("Ersparnis_Scooter", 3);
+        model.addAttribute("Distanz_min", 3);
+        model.addAttribute("Distanz_max", 3);
+        model.addAttribute("Zeit_min", 3);
+        model.addAttribute("Zeit_max", 3);
+        model.addAttribute("Geschwindigkeit_min", 3);
+        model.addAttribute("Geschwindigkeit_max", 3);
+
+
         return "dashboard";
     }
 }
