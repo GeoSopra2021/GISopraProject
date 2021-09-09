1a708e83ef3717a8c2ff93261003d0ce19da4e87
Maike Schröder <maikeschroder@wl-pool2-pnt-01318.uni-muenster.de>
2021-09-09 11:08:53 +0200

    Zeitfilter

---
+++ trettidashboard/src/main/java/org/geosopra/ZeitFilter.java
@@ -0,0 +1,26 @@
+package org.geosopra;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZeitFilter {
+
+    public Datapoint[] filter(Datapoint[] dp, double begin, double end){
+        List<Datapoint> gefiltert = new ArrayList<>();
+        for(int i = 0; i < dp.length; i++) {
+            if (dp[i].getStartTime() >= begin && dp[i].getEndTime() <= end){
+                gefiltert.add(dp[i]);
+
+            }
+        }
+        Datapoint[] gefiltertesArray = new Datapoint[gefiltert.size()];
+        for(int i = 0; i < gefiltert.size(); i++) {
+            gefiltertesArray[i] = gefiltert.get(i);
+        }
+
+        return gefiltertesArray;
+    }
+
+
+}
