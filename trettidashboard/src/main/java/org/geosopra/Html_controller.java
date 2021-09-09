e2273a6f3ffd1d21998a0b10364b443781284e89
Timo Lietmeyer <48100977+Timo123456789@users.noreply.github.com>
2021-09-09 13:20:42 +0200

    add label for end and start time

--- trettidashboard/src/main/java/org/geosopra/Html_controller.java
+++ trettidashboard/src/main/java/org/geosopra/Html_controller.java
@@ -13,6 +13,7 @@ import org.springframework.ui.ConcurrentModel;
 import org.springframework.ui.Model;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -167,9 +168,29 @@ public class Html_controller {
 
     }
 
-    @RequestMapping("/")
-    public String welcome(Model model) {
+    /*@RequestMapping("/")
+    public String dashboard(Model model) {
 
+
+        model.addAllAttributes(save_model.asMap());
+        model.asMap().forEach((k, e) -> {
+            System.out.println(k);
+            System.out.println(e);
+        });
+        
+        return "dashboard";
+    }*/
+
+    @RequestMapping("/")
+    public String dashboard_with_filter(
+        @RequestParam(required = false, value= "trip-start") String start,
+        @RequestParam(required = false, value ="trip-end") String end, 
+        Model model
+    ) {
+        System.out.println("start");
+        System.out.println(start);
+        System.out.println("end");
+        System.out.println(end);
 
         model.addAllAttributes(save_model.asMap());
         model.asMap().forEach((k, e) -> {
