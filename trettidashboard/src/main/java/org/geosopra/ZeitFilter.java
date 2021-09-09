d3242b0b03aeca5b0f6137b2720a2a2d3a4094b4
Lorano98 <evalangstein@freenet.de>
2021-09-09 13:30:23 +0200

    Datapoint filter

--- trettidashboard/src/main/java/org/geosopra/ZeitFilter.java
+++ trettidashboard/src/main/java/org/geosopra/ZeitFilter.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 public class ZeitFilter {
 
-    public Datapoint[] filter(Datapoint[] dp, double begin, double end){
+    public Datapoint[] filter(Datapoint[] dp, long begin, long end){
         List<Datapoint> gefiltert = new ArrayList<>();
         for(int i = 0; i < dp.length; i++) {
             if (dp[i].getStartTime() >= begin && dp[i].getEndTime() <= end){
@@ -28,6 +28,23 @@ public class ZeitFilter {
     	
     	long unixTime = datum.getTime();
     	return unixTime;
+    }
+    
+    public Datapoint[] datumabfrage(Datapoint[] dp, String begin, String end) {
+    	
+    	int yearBegin = Integer.parseInt(begin.substring(0, 3));
+    	int monthBegin = Integer.parseInt(begin.substring(5, 6));
+    	int dayBegin = Integer.parseInt(begin.substring(8, 9));
+    	
+    	int yearEnd = Integer.parseInt(begin.substring(0, 3));
+    	int monthEnd = Integer.parseInt(begin.substring(5, 6));
+    	int dayEnd = Integer.parseInt(begin.substring(8, 9));
+    	
+    	long unixBegin = umrechnen(yearBegin, monthBegin, dayBegin);
+    	long unixEnd = umrechnen(yearEnd, monthEnd, dayEnd);
+    	
+    	return filter(dp, unixBegin, unixEnd);
+    	
     }
 
 
